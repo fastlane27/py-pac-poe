810b3887c395470e06427163d02dc7902373ae3d
David Lane <fastlane27@gmail.com>
2020-03-13 16:13:28 -0500

    Complete bonuses and refactor

--- py-pac-poe.py
+++ py-pac-poe.py
@@ -2,28 +2,53 @@ state = {}
 
 def play_game():
     init_game()
-    while not state['winner']:
+    get_win_count()
+    while True:
         print_board()
-        handle_move(get_move())
-        handle_turn()
-        check_winner()
-    print_board()
-    handle_winner()
+        get_move()
+        if result():
+            print_board()
+            print_result()
+            set_score()
+            if is_win_count():
+                final_result()
+                return
+            else:
+                reset_game()
+        else:
+            switch_turn()
 
 def init_game():
     print(
-        '\n----------------------'
-        "\nLet's play Py-Pac-Poen"
-        '\n----------------------'
+        '\n-----------------------'
+        "\n Let's play Py-Pac-Poe"
+        '\n-----------------------'
     )
-    
+    state['win_count'] = 1
+    state['score'] = { 'X': 0, 'O': 0, 'T': 0 }
+    state['winner'] = None
+    reset_game()
+
+def reset_game():
     state['board'] = {
         'a1': ' ', 'b1': ' ', 'c1': ' ',
         'a2': ' ', 'b2': ' ', 'c2': ' ',
         'a3': ' ', 'b3': ' ', 'c3': ' '
     }
     state['turn'] = 'X'
-    state['winner'] = None
+
+def get_win_count():
+    while True:
+        try:
+            count = int(input('\nHow many wins would you like to play to? (max: 5): '))
+        except:
+            print('Please enter a valid number...')
+        else:
+            if count in range(1, 5):
+                state['win_count'] = count
+                return
+            else:
+                print('Please enter a number between 1 and 5...')
 
 def print_board():
     print(
@@ -42,17 +67,15 @@ def get_move():
     while True:
         move = input(f"Player {state['turn']}'s move (example C2): ").lower()
         if move in state['board'] and state['board'][move] == ' ':
-            return move
+            state['board'][move] = state['turn']
+            return
         else:
             print('Bogus move! Try again...\n')
-    
-def handle_move(move):
-    state['board'][move] = state['turn']
 
-def handle_turn():
+def switch_turn():
     state['turn'] = 'O' if state['turn'] == 'X' else 'X'
-
-def check_winner():
+    
+def result():
     win_list = [
         ['a1', 'b1', 'c1'],
         ['a2', 'b2', 'c2'],
@@ -63,18 +86,30 @@ def check_winner():
         ['a1', 'b2', 'c3'],
         ['a3', 'b2', 'c1']
     ]
-
     for pos in win_list:
         if (state['board'][pos[0]] != ' ' and 
                 state['board'][pos[0]] == state['board'][pos[1]] == state['board'][pos[2]]):
             state['winner'] = state['board'][pos[0]]
-            return
+            return state['winner']
     state['winner'] = None if ' ' in state['board'].values() else 'T'
+    return state['winner']
 
-def handle_winner():
+def print_result():
     if state['winner'] == 'T':
         print("It's a tie game!")
     else:
         print(f"Player {state['winner']} wins!")
+
+def set_score():
+    state['score'][state['winner']] += 1
+    print(f"Score -- X: {state['score']['X']} | O: {state['score']['O']} | Ties: {state['score']['T']}")
+
+def is_win_count():
+    if state['score']['X'] < state['win_count'] and state['score']['O'] < state['win_count']:
+        return False
+    return True
+
+def final_result():
+    print(f"\nCongrats to Player {state['winner']} for winning {state['score'][state['winner']]} games!")
 
 play_game()
