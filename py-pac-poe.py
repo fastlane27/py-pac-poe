cc27c55f5f333cdbd516480aab393d433323f272
David Lane <fastlane27@gmail.com>
2020-03-12 23:30:08 -0500

    first commit

---
+++ py-pac-poe.py
@@ -0,0 +1,44 @@
+import re
+
+state = {}
+
+def print_board():
+    print('\n       A   B   C\n\n'
+        f"   1   {state['board']['a1']} | {state['board']['b1']} | {state['board']['c1']}\n"
+        '      -----------\n'
+        f"   2   {state['board']['a2']} | {state['board']['b2']} | {state['board']['c2']}\n"
+        '      -----------\n'
+        f"   3   {state['board']['a3']} | {state['board']['b3']} | {state['board']['c3']}\n")
+
+def get_move():
+    move = input(f"Player {state['turn']}'s move (example B2): ").lower()
+
+    while not re.match('[A-Ca-c][1-3]', move):
+        print('Bogus move! Try again...\n')
+        move = input(f"Player {state['turn']}'s move (example B2): ").lower()
+
+    state['board'][move] = state['turn']
+    state['turn'] = 'O'
+    print_board()
+    check_winner()
+
+def check_winner():
+    get_move()
+
+def init_game():
+    print('\n----------------------\n'
+        "Let's play Py-Pac-Poe!\n"
+        '----------------------')
+    
+    state['board'] = {
+        'a1': ' ', 'b1': ' ', 'c1': ' ',
+        'a2': ' ', 'b2': ' ', 'c2': ' ',
+        'a3': ' ', 'b3': ' ', 'c3': ' '
+    }
+    state['turn'] = 'X'
+    state['winner'] = None
+    
+    print_board()
+    get_move()
+
+init_game()
