318d27d4587a9e034b1972d43d6a7fbd4aa62ddb
David Lane <fastlane27@gmail.com>
2020-03-13 02:33:03 -0500

    Win condition and refactor

--- py-pac-poe.py
+++ py-pac-poe.py
@@ -1,19 +1,21 @@
-import re
-
 state = {}
 
-def print_board():
-    print('\n       A   B   C\n\n'
-        f"   1   {state['board']['a1']} | {state['board']['b1']} | {state['board']['c1']}\n"
-        '      -----------\n'
-        f"   2   {state['board']['a2']} | {state['board']['b2']} | {state['board']['c2']}\n"
-        '      -----------\n'
-        f"   3   {state['board']['a3']} | {state['board']['b3']} | {state['board']['c3']}\n")
+def play_game():
+    init_game()
+    while not state['winner']:
+        print_board()
+        handle_move(get_move())
+        handle_turn()
+        check_winner()
+    print_board()
+    handle_winner()
 
 def init_game():
-    print('\n----------------------\n'
-        "Let's play Py-Pac-Poe!\n"
-        '----------------------')
+    print(
+        '\n----------------------'
+        "\nLet's play Py-Pac-Poen"
+        '\n----------------------'
+    )
     
     state['board'] = {
         'a1': ' ', 'b1': ' ', 'c1': ' ',
@@ -22,29 +24,57 @@ def init_game():
     }
     state['turn'] = 'X'
     state['winner'] = None
-    
-    print_board()
-    get_move()
+
+def print_board():
+    print(
+f"""
+      A   B   C
+
+  1   {state['board']['a1']} | {state['board']['b1']} | {state['board']['c1']}
+     -----------
+  2   {state['board']['a2']} | {state['board']['b2']} | {state['board']['c2']}
+     -----------
+  3   {state['board']['a3']} | {state['board']['b3']} | {state['board']['c3']}
+"""
+    )
 
 def get_move():
-    prompt = f"Player {state['turn']}'s move (example B2): "
+    while True:
+        move = input(f"Player {state['turn']}'s move (example C2): ").lower()
+        if move in state['board'] and state['board'][move] == ' ':
+            return move
+        else:
+            print('Bogus move! Try again...\n')
     
-    move = input(prompt).lower()
-    while not check_move(move):
-        move = input(prompt).lower()
-
+def handle_move(move):
     state['board'][move] = state['turn']
-    print_board()
-    check_winner()
+
+def handle_turn():
     state['turn'] = 'O' if state['turn'] == 'X' else 'X'
 
-def check_move(move):
-    if not re.match('[A-Ca-c][1-3]', move) or state['board'][move] != ' ':
-        print('Bogus move! Try again...\n')
-        return False
-    return True
-
 def check_winner():
-    get_move()
+    win_list = [
+        ['a1', 'b1', 'c1'],
+        ['a2', 'b2', 'c2'],
+        ['a3', 'b3', 'c3'],
+        ['a1', 'a2', 'a3'],
+        ['b1', 'b2', 'b3'],
+        ['c1', 'c2', 'c3'],
+        ['a1', 'b2', 'c3'],
+        ['a3', 'b2', 'c1']
+    ]
 
-init_game()
+    for pos in win_list:
+        if (state['board'][pos[0]] != ' ' and 
+                state['board'][pos[0]] == state['board'][pos[1]] == state['board'][pos[2]]):
+            state['winner'] = state['board'][pos[0]]
+            return
+    state['winner'] = None if ' ' in state['board'].values() else 'T'
+
+def handle_winner():
+    if state['winner'] == 'T':
+        print("It's a tie game!")
+    else:
+        print(f"Player {state['winner']} wins!")
+
+play_game()
